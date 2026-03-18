a417d3a9f07df244619da1bc4d1d362d3191546a
Oscar Leiva <os.leivac@duocuc.cl>
2026-03-18 20:46:44 -0300

    feat: Add initial configuration files for session and private key

--- EA1/ACT1.2/provider.tf
+++ EA1/ACT1.2/provider.tf
@@ -1,3 +1,16 @@
+/*provider "aws" {
+  region = "us-east-1"
+}
+*/
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
